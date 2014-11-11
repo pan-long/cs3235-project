d9e9f9f6b7820a85accf950089bbc6eba88bc138
Pan Long <aga.panlong@me.com>
2014-11-11 18:33:44 +0800

    format the code in leap painter

--- logic/leap_painter.py
+++ logic/leap_painter.py
@@ -7,16 +7,28 @@ from storage.preference import Arguments
 
 class LeapPainter():
 	lastFrameId = 0
+
+	# has the gesture started
 	started = False
+
+	# is currently pressed?
 	isPressed = False
-	point_coord = []
+
 	isLeftHand = False
+
+	# how many consecutive plain frames we have?
+	# if it hits a certain number, we should stop gesturing and start verifying
 	idleCounter = 0
-	lastIsIdle = False
+
+	# all the points we have stores in gestures
 	points = []
+	lines = []
 
 	def draw(self, x, y, width, height, color):
 		self.paintCanvas.create_oval(x, y, x + width, y + height, fill = color, outline = "")
+
+	def draw_line(self, x1, y1, x2, y2, width, color):
+		self.paintCanvas.create_line(x1, y1, x2, y2, width = width, dash = True)
 
 	def set_canvas(self, canvas):
 		self.paintCanvas = canvas
@@ -27,54 +39,72 @@ class LeapPainter():
 	def getFrame(self, controller):
 		self.currentFrame = controller.frame()
 
+	def redraw(self):
+		# any process of frame from Leap should goes here
+		self.paintCanvas.delete("all")
+		# redraw the background image
+		if Arguments.isUsingPictureMode:
+			self.image = ImageTk.PhotoImage(Image.open(Arguments.picturePath).resize((800, 600)))
+			self.paintCanvas.create_image(0, 0, image = self.image, anchor = NW)
+		
+		# redraw what we have stored in our gesture
+		if Arguments.isUsingPictureMode or Arguments.isUsingGestureMode:
+			color = self.rgb_to_hex((200, 0, 0))
+			for point in self.points:
+				self.draw(point.x * 800, 600 - point.y, 40, 40, color)
+
+		# redraw lines that we have stored
+		if Arguments.isUsingPictureMode:
+			color = self.rgb_to_hex((160, 0, 0))
+			for line in self.lines:
+				self.draw_line(line[0].x, line[0].y, line[1].x, line[1].y, 10, color)	
+
+	def processingPictureMode(self, frame):
+		# TO-DO
+		return
+
+	def processingBinaryMode(self, frame):
+		# TO-DO
+		return 
+
+	def processingGestureMode(self, frame):
+		# TO-DO
+		return
+
+	def verify(self):
+		# TO-DO
+		return True
+
 	def processFrame(self):
 		if self.currentFrame.id == self.lastFrameId:
 			# if we requesting the frame too fast and get the same frame as last requesting,
 			# simply ignore this frame as we have processed already
+			self.idleCounter += 1
 			return
 		else:
-			# any process of frame from Leap should goes here
-			self.paintCanvas.delete("all")
-			# redraw the background image
-			if Arguments.isUsingPictureMode:
-				self.image = ImageTk.PhotoImage(Image.open(Arguments.picturePath).resize((800, 600)))
-				self.paintCanvas.create_image(0, 0, image = self.image, anchor = NW)
-			
+			self.redraw()
+
 			frame = self.currentFrame
-			interactionBox = frame.interaction_box
+			# if the frame is plain, simply add counter
+			if len(frame.pointables) == 0:
+				self.idleCounter += 1
+				if self.idleCounter >= 30:
+					# do verification and display something secret
+					sys.exit(0)
+				return  
 
-			# redraw existing points
-			if Arguments.isUsingPictureMode or Arguments.isUsingGestureMode:
-				color = self.rgb_to_hex((200, 0, 0))
-				for point in self.points:
-					if point[0] > 0 and point[1] > 0: 
-						self.draw(point[0] * 800, 600 - point[1]* 600, 40, 40, color)
-			
-			if Arguments.isUsingPictureMode and len(frame.pointables) > 0:
-				deepestPoint = frame.hands[0].fingers.frontmost
-				normalizedPosition = interactionBox.normalize_point(deepestPoint.tip_position)
+			# if this frame is not plain, reset the counter to 0
+			self.idleCounter = 0
 
-				if(deepestPoint.touch_distance > 0 and deepestPoint.touch_zone != Leap.Pointable.ZONE_NONE):
-					color = self.rgb_to_hex((0, 255 - 255 * deepestPoint.touch_distance, 0))
-					
-				elif(deepestPoint.touch_distance <= 0):
-					color = self.rgb_to_hex((-255 * deepestPoint.touch_distance, 0, 0))
-					
-				else:
-					color = self.rgb_to_hex((0,0,200))
-				
-				# if normalizedPosition.x is not greater than 0, the points will not 
-				# disapear in the canvas, but simply go along the edge
-				if normalizedPosition.x > 0 and normalizedPosition.y > 0: 
-					self.draw(normalizedPosition.x * 800, 600 - normalizedPosition.y * 600, 40, 40, color)
+			if Arguments.isUsingPictureMode:
+				self.processingPictureMode(frame)
+			elif Arguments.isUsingBinaryMode:
+				self.processingBinaryMode(frame)
+			elif Arguments.isUsingGestureMode:
+				self.processingGestureMode(frame)
+			else:
+				print 'No mode specified, using picture mode by default'
+				self.processingPictureMode(frame)
 
-			if Arguments.isUsingBinaryMode:
-				# if it is using left/right hand mode, simply use binary value for left/right hand, 
-				# any hand present will result in "pressed" status
-				if len(frame.hands) == 0:
-					# if no hand present 
-					if not lastIsIdle:
-						points.append(isLeftHand)
-						idleCounter += 1
-
+			# set current frame id as the last frame id
 			self.lastFrameId = frame.id
